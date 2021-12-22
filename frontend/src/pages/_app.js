b1ace8cbbf49e3892ca939d105b8a31872fa7489
Amor Kumar <contact@amorkumar.com>
2021-12-22 02:04:41 -0500

    Add a loading bar

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,7 +1,17 @@
 import '@/styles/globals.css'
+import NextNProgress from 'nextjs-progressbar'
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../tailwind.config'
+
+const colors = resolveConfig(tailwindConfig).theme.colors
 
 function MyApp({ Component, pageProps = {} }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress color={colors.black} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
