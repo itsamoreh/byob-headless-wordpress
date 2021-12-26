9c01529859d19b21ccdf86097f004352fa9bde18
Amor Kumar <contact@amorkumar.com>
2021-12-26 11:35:58 -0500

    Add comment

--- backend/wordpress/wp-content/themes/byob/index.php
+++ backend/wordpress/wp-content/themes/byob/index.php
@@ -10,7 +10,7 @@
 /**
  * Redirect editors to the headless frontend.
  */
-if ( defined('FRONTEND_APP_URL') ) {
+if ( defined('FRONTEND_APP_URL') ) { // Defined in byob-core plugin
 	header( 'X-Redirect-By: BYOB Headless WordPress Theme' );
 	header( 'Location: ' . FRONTEND_APP_URL . $_SERVER[REQUEST_URI], true, $statusCode );
 	die();
@@ -23,6 +23,6 @@ if ( defined('FRONTEND_APP_URL') ) {
 	<div>
 		<h1>Redirection Error</h1>
 		<p>There was a problem redirecting you to the headless frontend. Please manually navigate there to see your changes.</p>
-		<p><small>Make sure to define <code>FRONTEND_APP_URL</code> in the BYOB plugin.</small></p>
+		<p><small>Make sure to define <code>FRONTEND_APP_URL</code> in the BYOB Core plugin.</small></p>
 	</div>
 </main><!-- #primary -->
