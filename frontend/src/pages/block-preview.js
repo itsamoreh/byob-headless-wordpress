9085eb65b1f6bda07ecdaf23271dd7442c62404e
Amor Kumar <contact@amorkumar.com>
2021-12-26 11:33:01 -0500

    Add block preview page

--- frontend/src/pages/block-preview.js
+++ frontend/src/pages/block-preview.js
@@ -1,12 +1,16 @@
 import displayBlock from '@/functions/wordpress/display-block'
+import camelizeKeys from '@/lib/camelize-keys'
 
 export default function Block({ block }) {
-  console.log(block)
   return <div>{displayBlock(block)}</div>
 }
 
 export async function getServerSideProps(context) {
   const block = context.query
+
+  if (block?.acfAttributes) {
+    block.acfAttributes = camelizeKeys(JSON.parse(block.acfAttributes))
+  }
 
   return {
     props: {
