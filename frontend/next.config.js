9da5c1c0c3b4d87c1a2064a578da7b23fbc78829
Amor Kumar <contact@amorkumar.com>
2021-12-30 23:57:18 -0500

    Redirect nextjs /wp-admin to wordpress

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/wp-admin/:path*',
+        destination: `${process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL}/wp-admin/:path*`,
+      },
+    ]
+  },
+}
