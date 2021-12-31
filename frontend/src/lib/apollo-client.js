fb35db86956c91a13953ae68fbc60adb5603a4dd
Amor Kumar <contact@amorkumar.com>
2021-12-30 23:55:13 -0500

    Remove useless comments

--- frontend/src/lib/apollo-client.js
+++ frontend/src/lib/apollo-client.js
@@ -2,20 +2,12 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
 let client
 
-/**
- * getApolloClient
- */
-
 export function getApolloClient() {
   if (!client) {
     client = _createApolloClient()
   }
   return client
 }
-
-/**
- * createApolloClient
- */
 
 export function _createApolloClient() {
   return new ApolloClient({
