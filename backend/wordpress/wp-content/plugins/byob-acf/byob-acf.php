355d572579fe780d3a459bfe8bcfd9413e90a535
Amor Kumar <contact@amorkumar.com>
2021-12-30 23:53:43 -0500

    Format and address warnings

--- backend/wordpress/wp-content/plugins/byob-acf/byob-acf.php
+++ backend/wordpress/wp-content/plugins/byob-acf/byob-acf.php
@@ -23,29 +23,29 @@
  * @param array $block The block being previewed.
  */
 function byob_block_render_callback( $block ) {
-	$frontend_url = FRONTEND_APP_URL; // Defined in byob-core plugin
-	$attributes = get_fields();
-	$attributes_encoded = urlencode(json_encode($attributes));
+	$frontend_url       = FRONTEND_APP_URL; // Defined in byob-core plugin.
+	$attributes         = get_fields();
+	$attributes_encoded = rawurlencode( wp_json_encode( $attributes ) );
 
-
-	if( !empty($frontend_url) ): ?>
+	if ( ! empty( $frontend_url ) ) : ?>
 		<div style="position: relative; width: 100%; height: 100%; pointer-events: none;">
 			<iframe
 				class="blockPreviewIframe"
-				src="<?php echo($frontend_url . '/block-preview?name=' . $block['name'] . '&acfAttributes=' . $attributes_encoded); ?>"
-				title="<?php echo($block['name'] . ' Preview'); ?>"
+				src="<?php echo( esc_url( $frontend_url . '/block-preview?name=' . $block['name'] . '&acfAttributes=' . $attributes_encoded ) ); ?>"
+				title="<?php echo( esc_attr( $block['name'] ) . ' Preview' ); ?>"
 			></iframe>
 		</div>
 		<script>
 			iFrameResize({ log: false, resizeFrom: "child", heightCalculationMethod: "max" }, ".blockPreviewIframe")
 		</script>
-	<?php else: ?>
+	<?php else : ?>
 		<div style="padding: 24px; border: 2px solid firebrick;">
 			<h2 style="margin-top: 0; color: firebrick;">Preview Unavailable</h2>
 			<p>Activate the BYOB Core Plugin to see block previews. <br/></p>
 			<p style="margin-bottom: 0;"><small><code>FRONTEND_APP_URL</code> is not defined.</small></p>
 		</div>
-	<?php endif;
+		<?php
+	endif;
 }
 
 /**
@@ -55,8 +55,11 @@ function byob_block_render_callback( $block ) {
  */
 function byob_enqueue_iframe_resizer() {
 	wp_enqueue_script(
-			'byob-iframe-resizer',
-			plugins_url( 'lib/iframe-resizer.js', __FILE__ )
+		'byob-iframe-resizer',
+		plugins_url( 'lib/iframe-resizer.js', __FILE__ ),
+		array(),
+		'1.0.0',
+		true
 	);
 }
 add_action( 'enqueue_block_editor_assets', 'byob_enqueue_iframe_resizer' );
@@ -64,13 +67,13 @@ add_action( 'enqueue_block_editor_assets', 'byob_enqueue_iframe_resizer' );
 /**
  * Include custom ACF block types.
  */
-foreach( glob( dirname( __FILE__ ) . "/blocks/*.php" ) as $block){
+foreach ( glob( dirname( __FILE__ ) . '/blocks/*.php' ) as $block ) {
 	require $block;
 }
 
 /**
  * Include ACF field groups.
  */
-foreach( glob( dirname( __FILE__ ) . "/fields/*.php" ) as $fields){
+foreach ( glob( dirname( __FILE__ ) . '/fields/*.php' ) as $fields ) {
 	require $fields;
 }
