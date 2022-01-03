0c7175c9afcf66d2b2e42f7812c652f73e4bc9f2
Amor Kumar <contact@amorkumar.com>
2022-01-03 02:33:51 -0500

    Add author page

---
+++ frontend/src/pages/posts/author/[authorSlug].js
@@ -0,0 +1,136 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import { getApolloClient } from '@/api/apollo-client'
+import { gql } from '@apollo/client'
+
+export default function AuthorSingle({ author, site }) {
+  return (
+    <div>
+      <Head>
+        <title>Posts by {author.name}</title>
+        <meta
+          name="description"
+          content={`Read more about ${author.name} on ${site.title}`}
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className="bg-indigo-50">
+        <div className="flex flex-col max-w-5xl px-6 py-8 mx-auto space-y-8 md:space-y-0 md:items-center md:justify-between md:flex-row">
+          <div className="max-w-md mx-auto md:max-w-none">
+            <h1 className="text-3xl font-extrabold leading-tight break-words lg:text-5xl">
+              by{' '}
+              <span className="text-4xl text-indigo-600 lg:text-6xl">
+                {author?.name}
+              </span>
+            </h1>
+
+            {author.description && (
+              <p className="mt-2 text-sm text-gray-600 lg:text-base">
+                {author?.description}
+              </p>
+            )}
+          </div>
+
+          {author?.avatar?.url && (
+            <img
+              className="md:ml-16 rounded-full mx-auto object-cover w-52 lg:w-72 aspect-[1/1] shadow-md"
+              src={author.avatar.url}
+              alt={`Picture of ${author?.name}`}
+            />
+          )}
+        </div>
+      </header>
+
+      <div className="container pb-16">
+        <div className="mx-auto prose prose-indigo">
+          <hr />
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export async function getStaticProps({ params = {} } = {}) {
+  const { authorSlug } = params
+
+  const apolloClient = getApolloClient()
+
+  const data = await apolloClient.query({
+    query: gql`
+      ${AUTHOR_FIELDS}
+      query AuthorBySlug($slug: ID!) {
+        generalSettings {
+          title
+        }
+        user(id: $slug, idType: SLUG) {
+          ...AuthorFields
+        }
+      }
+    `,
+    variables: {
+      slug: authorSlug,
+    },
+  })
+
+  const author = data?.data.user
+
+  const site = {
+    ...data?.data.generalSettings,
+  }
+
+  return {
+    props: {
+      author,
+      site,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const apolloClient = getApolloClient()
+
+  const data = await apolloClient.query({
+    query: gql`
+      {
+        users(first: 10000) {
+          edges {
+            node {
+              id
+              slug
+              name
+            }
+          }
+        }
+      }
+    `,
+  })
+
+  const authors = data?.data.users.edges.map(({ node }) => node)
+
+  return {
+    paths: authors.map(({ slug }) => {
+      return {
+        params: {
+          authorSlug: slug,
+        },
+      }
+    }),
+    fallback: false,
+  }
+}
+
+export const AUTHOR_FIELDS = gql`
+  fragment AuthorFields on User {
+    uri
+    name
+    description
+    avatar(size: 500) {
+      url
+    }
+  }
+`
