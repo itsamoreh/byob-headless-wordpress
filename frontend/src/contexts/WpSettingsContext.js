4becb17345fa0d8a94da0e19db78a2bbe5e8db91
Amor Kumar <contact@amorkumar.com>
2022-01-03 20:45:49 -0500

    Remove settings, set more defaults

--- frontend/src/contexts/WpSettingsContext.js
+++ frontend/src/contexts/WpSettingsContext.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 export const WpSettingsContext = React.createContext({
   generalSettingsDateFormat: 'F j, Y',
-  generalSettingsDescription: '',
-  generalSettingsLanguage: '',
+  generalSettingsDescription: 'Just another WordPress site',
   generalSettingsStartOfWeek: 1,
   generalSettingsTimeFormat: 'g:i a',
-  generalSettingsTimezone: '',
-  generalSettingsTitle: '',
+  generalSettingsTimezone: 'America/Toronto',
+  generalSettingsTitle: 'BYOB',
   readingSettingsPostsPerPage: 10,
   writingSettingsDefaultCategory: 1,
-  writingSettingsDefaultPostFormat: '',
 })
