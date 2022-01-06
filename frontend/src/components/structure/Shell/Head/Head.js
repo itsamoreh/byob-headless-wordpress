e7d68fb7b89f0139a139ee40ecaf1f732d842266
Amor Kumar <contact@amorkumar.com>
2022-01-06 01:09:20 -0500

    Add favicons and app icons

--- frontend/src/components/structure/Shell/Head/Head.js
+++ frontend/src/components/structure/Shell/Head/Head.js
@@ -6,8 +6,20 @@ import { gql } from '@apollo/client'
 export default function Head({ seo, manualSeo }) {
   return (
     <NextHead>
-      <link rel="icon" href="/favicon.ico" />
-
+      {
+        // @see https://realfavicongenerator.net/
+        // prettier-ignore
+        <>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4f46e5" />
+          <meta name="msapplication-TileColor" content="#603cba" />
+          <meta name="theme-color" content="#4f46e5" />
+        </>
+      }
       {seo && (
         <>
           <title>{seo.title}</title>
