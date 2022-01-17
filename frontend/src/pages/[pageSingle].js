3d666f529407db12dbf5f4447ee94d251c0a8f44
Amor Kumar <contact@amorkumar.com>
2022-01-17 00:51:09 -0500

    Add page title to post single

--- frontend/src/pages/[pageSingle].js
+++ frontend/src/pages/[pageSingle].js
@@ -17,6 +17,9 @@ export default function Post({ page, menus, wpSettings }) {
   return (
     <Shell wpSettings={wpSettings} menus={menus} seo={page.seo}>
       <main>
+        <h1 className="mb-8 text-6xl font-extrabold leading-tight text-center break-words">
+          {page.title}
+        </h1>
         <BlockRenderer blocks={page.blocks} />
       </main>
     </Shell>
