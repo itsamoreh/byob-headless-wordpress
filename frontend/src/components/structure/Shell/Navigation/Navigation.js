c9e0475e5622d747352a0102e74929680d81ab01
Amor Kumar <contact@amorkumar.com>
2022-01-22 22:17:16 -0500

    Add mobile nav open close states

--- frontend/src/components/structure/Shell/Navigation/Navigation.js
+++ frontend/src/components/structure/Shell/Navigation/Navigation.js
@@ -3,13 +3,17 @@ import { trimEnd } from 'lodash'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 export default function Navigation({ menuItems, wpSettings }) {
-  const router = useRouter()
   const { title } = wpSettings
+
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const router = useRouter()
+
   return (
     <nav className="container z-50 flex flex-wrap items-center justify-between p-6 mb-12 bg-transparent">
-      <div className="flex items-center flex-shrink-0 mr-6">
+      <div className="z-50 flex items-center flex-shrink-0 mr-6">
         <Link href="/">
           <a>
             <span className="text-xl font-black tracking-tight text-indigo-600">
@@ -18,8 +22,13 @@ export default function Navigation({ menuItems, wpSettings }) {
           </a>
         </Link>
       </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 text-indigo-600 hover:opacity-80">
+      <div className="z-50 block lg:hidden">
+        <button
+          className={cn(
+            'flex items-center px-3 py-2 text-indigo-600 hover:opacity-80'
+          )}
+          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+        >
           <svg
             className="w-3 h-3 fill-current"
             viewBox="0 0 20 20"
@@ -30,12 +39,18 @@ export default function Navigation({ menuItems, wpSettings }) {
           </svg>
         </button>
       </div>
-      <div className="flex-grow block w-full lg:flex lg:items-center lg:w-auto">
-        <div className="lg:flex-grow">
+      <div
+        className={cn(
+          'absolute inset-0 px-6 pt-[4.75rem] bg-white flex-grow block w-full',
+          'lg:static lg:mt-0 lg:px-0 lg:pt-0 lg:bg-transparent lg:flex lg:items-center lg:w-auto',
+          mobileMenuOpen ? 'block' : 'hidden'
+        )}
+      >
+        <div className="lg:flex-grow lg:flex">
           {menuItems &&
             menuItems.map((item, n) => (
               <Link key={n} href={item.path} target={item.target || ''}>
-                <a className="block mt-4 mr-4 font-medium text-indigo-600 transition-opacity duration-500 text-md lg:inline-block lg:mt-0 group">
+                <a className="block mt-4 mr-4 font-medium text-indigo-600 transition-opacity duration-500 text-md lg:mt-0 group">
                   <span
                     className={cn(
                       'opacity-80 link-underline link-underline-indigo-600 group-hover:opacity-100',
