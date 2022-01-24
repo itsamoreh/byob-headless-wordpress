5988d85712387f330e2112a26a6cf5bef2557706
Amor Kumar <contact@amorkumar.com>
2022-01-24 00:16:32 -0500

    Show the static homepage if set

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,56 +1,92 @@
 import { uniqBy } from 'lodash'
 
 import { getApolloClient } from '@/api/apollo-client'
+import BlockRenderer from '@/components/blocks/BlockRenderer'
+import { CALL_TO_ACTION_FIELDS } from '@/components/blocks/CallToAction/CallToAction'
+import { FREEFORM_FIELDS } from '@/components/blocks/Freeform/Freeform'
 import PostCard from '@/components/global/PostCard'
 import { POST_CARD_FIELDS } from '@/components/global/PostCard/PostCard'
 import Shell from '@/components/structure/Shell'
 import { FOOTER_MENU } from '@/components/structure/Shell/Footer/Footer'
+import { PAGE_SEO_FIELDS } from '@/components/structure/Shell/Head/Head'
 import { NAVIGATION_MENU } from '@/components/structure/Shell/Navigation/Navigation'
 import { WP_SETTINGS_FIELDS } from '@/components/structure/Shell/Shell'
+import { PAGE_FIELDS } from '@/pages/[pageSingle]'
 import { gql } from '@apollo/client'
 
-export default function Home({ posts, menus, wpSettings }) {
-  return (
-    <Shell
-      wpSettings={wpSettings}
-      menus={menus}
-      manualSeo={{
-        title: `Blog - ${wpSettings.title}`,
-        description: 'Blog Description',
-      }}
-    >
-      <main className="mb-8 lg:my-16">
-        <ul className="container max-w-2xl">
-          {posts &&
-            posts.length > 0 &&
-            posts.map((post) => {
-              return (
-                <li key={post.id}>
-                  <PostCard {...post} />
-                </li>
-              )
-            })}
+export default function Home({ homepage, posts, menus, wpSettings }) {
+  if (homepage) {
+    return (
+      <Shell wpSettings={wpSettings} menus={menus} seo={homepage.seo}>
+        <main>
+          <h1 className="mb-8 text-6xl font-extrabold leading-tight text-center break-words">
+            {homepage.title}
+          </h1>
+          <BlockRenderer blocks={homepage.blocks} />
+        </main>
+      </Shell>
+    )
+  } else if (posts) {
+    return (
+      <Shell
+        wpSettings={wpSettings}
+        menus={menus}
+        manualSeo={{
+          title: `Blog - ${wpSettings.title}`,
+          description: 'Blog Description',
+        }}
+      >
+        <main className="mb-8 lg:my-16">
+          <ul className="container max-w-2xl">
+            {posts &&
+              posts.length > 0 &&
+              posts.map((post) => {
+                return (
+                  <li key={post.id}>
+                    <PostCard {...post} />
+                  </li>
+                )
+              })}
 
-          {!posts ||
-            (posts.length === 0 && (
-              <li>
-                <p className="text-center">Oops, no posts found!</p>
-              </li>
-            ))}
-        </ul>
-      </main>
-    </Shell>
-  )
+            {!posts ||
+              (posts.length === 0 && (
+                <li>
+                  <p className="text-center">Oops, no posts found!</p>
+                </li>
+              ))}
+          </ul>
+        </main>
+      </Shell>
+    )
+  } else {
+    return '' // TODO: forward to 404 page
+  }
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const apolloClient = getApolloClient()
 
   const response = await apolloClient.query({
     query: gql`
-      ${POST_CARD_FIELDS}
+      ${PAGE_FIELDS}
+      ${PAGE_SEO_FIELDS}
+      ${CALL_TO_ACTION_FIELDS}
+      ${FREEFORM_FIELDS}
       ${WP_SETTINGS_FIELDS}
-      query PostList {
+      ${POST_CARD_FIELDS}
+      query HomePage {
+        homepage {
+          ...PageFields
+          ...PageSeoFields
+          blocks {
+            ... on CoreFreeformBlock {
+              ...FreeformFields
+            }
+            ... on AcfByobCallToActionBlock {
+              ...CallToActionFields
+            }
+          }
+        }
         posts(first: 10) {
           edges {
             node {
@@ -74,6 +110,8 @@ export async function getStaticProps() {
       }
     `,
   })
+
+  const homepage = response?.data.homepage
 
   let posts = response?.data.posts.edges
     .map(({ node }) => node)
@@ -107,11 +145,6 @@ export async function getStaticProps() {
     : null
 
   return {
-    props: {
-      posts,
-      menus,
-      wpSettings,
-    },
-    revalidate: 10,
+    props: { homepage, posts, menus, wpSettings },
   }
 }
